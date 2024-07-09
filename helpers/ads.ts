dd91326c3bff35c9311ec51614c7896f89f57ee6
siparat <akimov1712@yandex.ru>
2024-07-09 12:05:13 +0300

    fix(app): Bug with long launch of browser extension

--- helpers/ads.ts
+++ helpers/ads.ts
@@ -1,12 +1,22 @@
 export const adBlockIsEnabled = async (): Promise<boolean> => {
+	const sleep = (ms: number) => {
+		return new Promise((res) => {
+			setTimeout(() => res(null), ms);
+		});
+	}
+
 	try {
 		const domain = process.env.NEXT_PUBLIC_TEST_ADS_DOMAIN;
 		if (!domain) {
 			return true;
 		}
-		await fetch(domain, { method: 'HEAD' });
+		for (let i = 0; i <= 3; i++) {
+			await sleep(1000);
+			await fetch(domain, { method: 'HEAD' });
+		}
 		return false;
 	} catch (error) {
+		console.log('error', error);
 		return true;
 	}
 };
