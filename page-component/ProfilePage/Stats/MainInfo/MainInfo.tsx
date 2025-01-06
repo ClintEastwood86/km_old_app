6d62a0ea1aeb63918cfe2ad4fc53eab4263e1a10
siparat <akimov1712@yandex.ru>
2025-01-06 17:02:28 +0300

    refactor(app): Changed display of the viewing time

--- page-component/ProfilePage/Stats/MainInfo/MainInfo.tsx
+++ page-component/ProfilePage/Stats/MainInfo/MainInfo.tsx
@@ -1,17 +1,13 @@
 import { SettingItem } from '@/components';
 import styles from './MainInfo.module.css';
 import { MainInfoProps } from './MainInfo.props';
-import dayjs from 'dayjs';
 
 export const MainInfo = ({ id, ranks, rankId, watchedMinutes, multiplier }: MainInfoProps) => {
 	return (
 		<div className={styles.statsSettingItems}>
 			<SettingItem title="ID" value={id.toString()} />
 			<SettingItem title="Ранг" value={ranks.length ? ranks[rankId - 1].name : 'Неизвестный'} />
-			<SettingItem
-				title="Просмотрено"
-				value={dayjs(new Date(0, 0, 0, 0, watchedMinutes)).format((watchedMinutes / 60 >= 24 ? 'DDд ' : '') + 'HHч mmм')}
-			/>
+			<SettingItem title="Просмотрено" value={`${Math.floor(watchedMinutes / 60)}ч ${watchedMinutes % 60}м`} />
 			<SettingItem link="/blog/bonus" title="Множитель" value={`${multiplier}x`} />
 		</div>
 	);
